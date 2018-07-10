3df725e34e3d85379d35481df5ad2969132fdbb9
Terrence Tang <thtang28@gmail.com>
2018-07-09 21:39:19 -0700

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView,ListView,
+                                    DetailView,CreateView,
+                                    UpdateView,DeleteView)
+from blog.models import Post,Comment
+from blog.forms import PostForm,CommentForm
+from django.urls import reverse_lazy
+# Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    # method for bd query
+    def get_queryset(self):
+        # field lookups
+        # lte is less than or equal to
+        # -published_date desc
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
